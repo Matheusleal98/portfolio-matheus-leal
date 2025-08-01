14246368fd534c2a04fb3796c8f9d4d3816ef7e3
Matheus Leal <matheussleal98@gmail.com>
2025-08-01 16:03:24 -0300

    feat: add experience, skill and sectionTitle component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import { Header } from "./components/header/header";
 
 import "@/app/styles/home.scss";
+import { Experience } from "./components/experience/experience";
 
 export default function Home() {
   return (
      <main className="container">
       <Header />
+      <Experience />
     </main>
   );
 }
