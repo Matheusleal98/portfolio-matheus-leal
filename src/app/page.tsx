f2f30ef2426907892267d6f422ee3e9a98dc1ad3
Matheus Leal <matheussleal98@gmail.com>
2025-08-01 16:16:11 -0300

    Feat: add information component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
-import { Header } from "./components/header/header";
-
 import "@/app/styles/home.scss";
+
+import { Header } from "./components/header/header";
 import { Experience } from "./components/experience/experience";
+import { Info } from "./components/infomation/information";
 
 export default function Home() {
   return (
      <main className="container">
       <Header />
       <Experience />
+      <Info />
     </main>
   );
 }
