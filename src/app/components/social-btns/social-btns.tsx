896f66ee107468f3f5886d132d58978abeee22a5
Matheus Leal <matheussleal98@gmail.com>
2025-08-01 16:33:17 -0300

    feat: add social btns components.

---
+++ src/app/components/social-btns/social-btns.tsx
@@ -0,0 +1,18 @@
+import { GitHubIcon } from "../icons/github-icon";
+import { LinkedInIcon } from "../icons/linkedin-icon";
+
+import "./social-btns.scss"
+
+
+export function SocialBtns(){
+    return(
+        <div className="social">
+            <a href="linkedin.com/in/matheusleal98/">
+                <LinkedInIcon/>
+            </a>
+            <a href="https://github.com/Matheusleal98">
+                <GitHubIcon/>
+            </a>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import "@/app/styles/home.scss";
 import { Header } from "./components/header/header";
 import { Experience } from "./components/experience/experience";
 import { Info } from "./components/infomation/information";
+import { SocialBtns } from "./components/social-btns/social-btns";
+import { EmailIcon } from "./components/icons/email-icon";
 
 export default function Home() {
   return (
@@ -11,6 +13,13 @@ export default function Home() {
       <Header />
       <Experience />
       <Info />
+      <div className="buttons">
+          <SocialBtns />
+          <a className="btn-primary" href="mailto:teste@gmail.com">
+            contact me
+            <EmailIcon />
+          </a>
+        </div>
     </main>
   );
 }
